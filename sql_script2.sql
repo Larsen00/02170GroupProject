8a7ddea37a3ed3b7b56eb58b3c51476f568ba773
hann7777 <114212384+hann7777@users.noreply.github.com>
2024-03-21 09:55:36 +0100

    Update sql_script2.sql
    function and event for calculating new prices

--- sql_script2.sql
+++ sql_script2.sql
@@ -1,4 +1,31 @@
 USE ISSUE_BANK;
 
+-- Function and event for calculating new prices 
+DELIMITER //
+CREATE FUNCTION randomPercentage() RETURNS DECIMAL(5,2)
+BEGIN
+    DECLARE rand_num DECIMAL(5,2);
+    
+    repeat
+    -- Generate a random number between -3% and 4%
+    SET rand_num = RAND() * 0.07 - 0.04;
+    until rand_num != 0 end repeat;
+    
+    RETURN rand_num;
+END//
+DELIMITER ;
 
+SET GLOBAL event_scheduler = 1;
+
+DELIMITER //
+
+CREATE EVENT new_prices
+ON SCHEDULE EVERY 1 day
+DO
+BEGIN
+    UPDATE prices
+    SET price = price * (1+randomPercentage());
+END//
+DELIMITER ;
+-- End of function and event for calculating new prices 
 
